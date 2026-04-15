69a6cf52a487cac80057aa73a59957c63d11f163
johanvis <johanviscarra@gmail.com>
2026-04-15 10:29:50 +0200

    Update API base to server (remove :5000)

--- kompensa/config.js
+++ kompensa/config.js
@@ -1,6 +1,5 @@
 window.KOMPENSA_CONFIG = window.KOMPENSA_CONFIG || {
-  // Byt denna när backend flyttas till riktig domän eller ny miljö.
-  API_BASE: "http://178.104.157.149:5000"
+  API_BASE: "http://178.104.157.149"
 };
 
 window.KOMPENSA_API_BASE = window.KOMPENSA_CONFIG.API_BASE;
