a2a4cf8e1ad17c3f50286cabf53d7089aafbb254
johanvis <johanviscarra@gmail.com>
2026-04-15 20:18:07 +0200

    Use api.videco.se

--- kompensa/config.js
+++ kompensa/config.js
@@ -1,5 +1,5 @@
 window.KOMPENSA_CONFIG = window.KOMPENSA_CONFIG || {
-  API_BASE: "http://178.104.157.149"
+  API_BASE: "https://api.videco.se"
 };
 
 window.KOMPENSA_API_BASE = window.KOMPENSA_CONFIG.API_BASE;
